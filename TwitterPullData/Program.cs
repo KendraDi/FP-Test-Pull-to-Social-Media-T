a5bbcd1f55c88458bc30dea50fb556a6d70ee9d8
KendraDi <kendra.diamond@yahoo.com>
2019-03-11 11:38:08 -0400

    Test Pull Data Attempt1

---
+++ TwitterPullData/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script;
+using System.Web.Script.Serialization;
+
+namespace TwitterPullData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var twitter = new Twitter
+            {
+                OAuthConsumerKey = "XXX",
+                OAuthConsumerSecret = "XXXX"
+            };
+            IEnumerable<string> tweets = twitter.GetTweets("user name", 10).Result;
+            foreach (var t in tweets)
+            {
+                Console.WriteLine(t+"\n");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
